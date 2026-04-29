69de4a2ab86d8a0e107156817a937ff9e6d106a8
nileshMeera123 <mitkari.nilesh9@gmail.com>
2026-04-29 16:04:29 +0530

    rectified

--- testCases/test_login.py
+++ testCases/test_login.py
@@ -14,7 +14,7 @@ class TestLogin:
     @pytest.mark.regression
     def test_login(self,setup):
         """]
-        dwd
+        dwdhhh
         """
         logger = MyLogger.getLogger()
         # print(get_username())
