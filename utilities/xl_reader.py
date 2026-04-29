1701a9ef0c46983c6a0af1b640c1f48787f1f7a4
nileshMeera123 <mitkari.nilesh9@gmail.com>
2026-04-29 13:18:49 +0530

    hello push the code

--- utilities/xl_reader.py
+++ utilities/xl_reader.py
@@ -3,6 +3,10 @@ import os
 
 
 def read_xl_data_to_list():
+    """
+
+    :return:
+    """
     test_data_path = os.path.join(os.path.abspath(os.curdir), "testdata", "login_data.xlsx")
     df = pd.read_excel(test_data_path)
     return df.values.tolist()
